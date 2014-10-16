859772c13c1db0ed3f65e78888d7dceb61eb35e8
Anuj Goyal <anuj.goyal@gmail.com>
2014-10-16 09:15:52 -0700

    [code] for gated project.

---
+++ gated/GatedCell.swift
@@ -0,0 +1,24 @@
+//
+//  GatedCell.swift
+//  gated
+//
+//  Created by Anuj Goyal on 10/16/14.
+//  Copyright (c) 2014 TXT2LRN. All rights reserved.
+//
+
+import UIKit
+
+class GatedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ gated/GatedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  gated
+//
+//  Created by Anuj Goyal on 10/16/14.
+//  Copyright (c) 2014 TXT2LRN. All rights reserved.
+//
+
+import UIKit
+
+class GatedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("GatedCell") as GatedCell
+        return cell
+    }
+    
+}
+
